7e8977855237bed228e0a302fc8a288b67f1a475
Vlad Korolev <vlad@v-lad.org>
2025-11-02 07:34:03 -0500

    Deployed 60f7b5c with MkDocs version: 1.6.1

---
+++ javascripts/mathjax.js
@@ -0,0 +1,19 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"], ['$', '$']],
+    displayMath: [["\\[", "\\]"], ['$$', '$$']],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    // Process all content on the page since math is in HTML paragraphs
+    ignoreHtmlClass: "no-mathjax"
+  }
+};
+
+document$.subscribe(() => {
+  MathJax.startup.output.clearCache()
+  MathJax.typesetClear()
+  MathJax.texReset()
+  MathJax.typesetPromise()
+})
